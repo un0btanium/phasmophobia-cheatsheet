b4dc132b0c2bbb48f1326ff2fff06578a2152a4b
un0btanium <unobtaniumlol@gmail.com>
2020-10-03 00:06:07 +0200

    add more voice lines and voice line categories

--- src/components/pages/voice-lines-page.component.js
+++ src/components/pages/voice-lines-page.component.js
@@ -43,26 +43,27 @@ export default class VoiceLinesPage extends Component {
 				{getCol(this.props.data.voicelines.general, index)}
 				{getCol(this.props.data.voicelines.ouija, index)}
 				{getCol(this.props.data.voicelines.spiritbox, index)}
+				{getCol(this.props.data.voicelines.attacktrigger, index)}
 			</Row>
 		}
 
-		if (this.state.width > 770) {
+		if (this.state.width > 1000) {
 			let list = [];
 			let index = 0;
-			let length = Math.max(this.props.data.voicelines.general.length, this.props.data.voicelines.ouija.length, this.props.data.voicelines.spiritbox.length);
+			let length = Math.max(this.props.data.voicelines.general.length, this.props.data.voicelines.ouija.length, this.props.data.voicelines.spiritbox.length, this.props.data.voicelines.attacktrigger.length);
 			while (index < length) {
 				list.push(addRowToList(index++));
 			}
 			
 			// HEADER ROW
-			let columnHeaders = ["General Questions", "Ouija Board Questions", "Spirit Box Questions"].map((title, index) => {
+			let columnHeaders = ["General Questions", "Ouija Board Questions", "Spirit Box Questions", "Anger and Attack Trigger Words"].map((title, index) => {
 				return <Col key={"header-" + title}><h3>{title}</h3></Col>
 			});
 
 
 			return <div style={{ display: "inline-block", width: "100%", height:"100%"}}>
 				<div style={{ display: "flex", alignItems: "center", justifyContent: "center", margin: "25px 0px 15px 0px" }}>
-					<Container style={{ padding:"0", margin:"20px"}}>
+					<Container style={{ padding:"0", margin:"20px", maxWidth: "90%"}}>
 						<Row style={{ width:"100%" }}>
 							{columnHeaders}
 						</Row>
@@ -72,8 +73,9 @@ export default class VoiceLinesPage extends Component {
 			</div>
 		} else {
 			let list = [];
-			let headerNames = { general: "General Questions", ouija: "Ouija Board Questions", spiritbox: "Spirit Box Questions"};
+			let headerNames = { general: "General Questions", ouija: "Ouija Board Questions", spiritbox: "Spirit Box Questions", attacktrigger: "Anger and Attack Trigger Words"};
 			for (let voicelineName in this.props.data.voicelines) {
+				console.log(voicelineName)
 				let voicelineList = [];
 				voicelineList.push(<Col key={"header-" + headerNames[voicelineName]} style={{ marginTop: "35px" }}><h3>{headerNames[voicelineName]}</h3></Col>);
 				for (let index = 0; index < this.props.data.voicelines[voicelineName].length; index++) {
@@ -84,8 +86,8 @@ export default class VoiceLinesPage extends Component {
 			}
 
 			return <div style={{ display: "inline-block", width: "100%", height:"100%"}}>
-				<div style={{ display: "flex", alignItems: "center", justifyContent: "center", margin: "25px 0px 15px 0px" }}>
-					<Container style={{ }}>
+				<div style={{ display: "flex", margin: "25px 0px 15px 0px" }}>
+					<Container style={{ marginLeft: "20px" }}>
 						{list}
 					</Container>
 				</div>
