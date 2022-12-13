a13296dec1e1ebf0da56f777e8c781c99086f861
Boti Kis <botond.kis@gmail.com>
2022-12-13 17:18:08 +0100

    Updated cold bois breathy breath

--- src/phasmophobia-data/data.js
+++ src/phasmophobia-data/data.js
@@ -225,7 +225,7 @@ var data = {
 			"secondaryEvidences": [
 				"Walking speed is affected by room temperature when hunting",
 				"1.4 m/s= 15°C+ | 1.75 m/s= 12°C | 2.1 m/s= 9°C | 2.3 m/s= 6°C | 2.5 m/s= 3°C | 2.7 m/s= 0°C",
-				"Shows their breath in room temperatures of 3°C or lower",
+				"Shows their breath on the ghost model in any room during hunts if the breaker is off",
 				"Has double the chance to turn off the breaker",
 				"Can no longer turn on the breaker, as this would stop them accelerating"
 			]
